7cba1ecd987923d38968e908708497e070650a65
Tymur Yavtushenko <temarych@gmail.com>
2023-02-16 10:50:23 +0200

    Fix reviews UI

--- src/modules/pages/Movie/ReviewsWidget/index.tsx
+++ src/modules/pages/Movie/ReviewsWidget/index.tsx
@@ -10,25 +10,15 @@ export interface ReviewsWidgetProps {
 export const ReviewsWidget = (props: ReviewsWidgetProps) => {
   return (
     <ReviewsWidget.Wrapper>
-      <ReviewsWidget.Container>
-        {props.reviews.map(review => (
-          <ReviewCard data={review} key={review.id} />
-        ))}
-      </ReviewsWidget.Container>
+      {props.reviews.map(review => (
+        <ReviewCard data={review} key={review.id} />
+      ))}
     </ReviewsWidget.Wrapper>
   );
 }
 
-ReviewsWidget.Container = styled.div`
+ReviewsWidget.Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  gap: 1.5em;
-`;
-
-ReviewsWidget.Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   gap: 1.5em;
 `;
