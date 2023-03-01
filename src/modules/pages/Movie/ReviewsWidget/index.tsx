dc06b3b5b429a1961abed007b56249381aaf11ca
Tymur Yavtushenko <temarych@gmail.com>
2023-03-01 18:00:54 +0200

    Update movie page UI

--- src/modules/pages/Movie/ReviewsWidget/index.tsx
+++ src/modules/pages/Movie/ReviewsWidget/index.tsx
@@ -29,6 +29,6 @@ export const ReviewsWidget = (props: ReviewsWidgetProps) => {
 
 ReviewsWidget.Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(18em, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
   gap: 1.5em;
 `;
