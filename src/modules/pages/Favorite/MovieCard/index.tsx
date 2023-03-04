819a2f195071ebd83eacb9802f1434833f2bc8ef
Tymur Yavtushenko <temarych@gmail.com>
2023-03-04 20:12:08 +0200

    Add favorite movie cards

---
+++ src/modules/pages/Favorite/MovieCard/index.tsx
@@ -0,0 +1,91 @@
+import { Card, IconButton, Rating, Typography } from "@mui/material";
+import { IAppState } from "@store/index";
+import { useGetMovieQuery } from "@store/reducers/movieApi";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { removeMovie as removeFavoriteMovieAction } from "@store/reducers/favorite";
+
+export interface MovieCardProps {
+  id: string;
+}
+
+export const MovieCard = (props: MovieCardProps) => {
+  const dispatch = useDispatch();
+
+  const mode = useSelector((state: IAppState) => state.config.mode);
+  const { data: movieData } = useGetMovieQuery(props.id);
+
+  if (!movieData) return <MovieCard.Wrapper variant="outlined" />;
+
+  const posterPath = movieData.poster_path ? `https://image.tmdb.org/t/p/w500/${movieData.poster_path}` : null;
+  const rating = movieData.vote_average / 10 * 5;
+
+  const removeMovie = (id: string) => dispatch(removeFavoriteMovieAction(id));
+
+  return (
+    <MovieCard.Wrapper variant="outlined">
+      <MovieCard.Image src={posterPath ?? undefined} />
+      <MovieCard.Container>
+        <Typography 
+          variant="h6" 
+          sx={{
+            overflow: "hidden",
+            display: "-webkit-box",
+            WebkitLineClamp: "1",
+            WebkitBoxOrient: "vertical",
+            height: "1.5em"
+          }}
+        >
+          {movieData.title}
+        </Typography>
+        <Rating
+          max={5}
+          precision={0.5}
+          value={rating}
+          readOnly
+          size="small"
+          sx={{
+            color: mode === "light" ? "black" : "white",
+            "& .MuiRating-iconEmpty": {
+              color: mode === "light" ? "black" : "white",
+            }
+          }}
+        />
+      </MovieCard.Container>
+      <IconButton 
+        size="large" 
+        sx={{ color: mode === "light" ? "black" : "white" }}
+        onClick={event => {
+          event.stopPropagation();
+          removeMovie(props.id);
+        }}
+      >
+        <FavoriteIcon sx={{ fontSize: "0.9em" }} />
+      </IconButton>
+    </MovieCard.Wrapper>
+  );
+}
+
+MovieCard.Image = styled.img`
+  border-radius: 1em;
+  object-fit: cover;
+  height: 100%;
+  aspect-ratio: 1;
+`;
+
+MovieCard.Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+MovieCard.Wrapper = styled(Card)`
+  border-radius: 1.5em !important;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1em;
+  padding: 1em;
+  height: 6em;
+`;
--- src/modules/pages/Favorite/index.tsx
+++ src/modules/pages/Favorite/index.tsx
@@ -1,24 +1,39 @@
 import { Typography } from "@mui/material";
-import { IAppState } from "@store/index";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { MovieCard } from "./MovieCard";
+import { IAppState } from "@store/index";
+import { useNavigate } from "react-router-dom";
 
 export const Favorite = () => {
+  const navigate = useNavigate();
   const movieIds = useSelector((state: IAppState) => state.favorite.movieIds);
 
   return (
     <Favorite.Wrapper>
       <Favorite.Container>
-        <Typography>
-          Favorite
-        </Typography>
-        {movieIds.map(movieId => (
-          <Typography key={movieId}>{movieId}</Typography>
-        ))}
+        <Favorite.MovieGrid>
+          {movieIds.map(movieId => (
+            <motion.div 
+              whileHover={{ scale: 1.025 }} 
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate(`/movie/${movieId}`)}
+            >
+              <MovieCard key={movieId} id={movieId} />
+            </motion.div>
+          ))}
+        </Favorite.MovieGrid>
       </Favorite.Container>
     </Favorite.Wrapper>
   );
 }
+
+Favorite.MovieGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
+  gap: 1.5em;
+`;
 
 Favorite.Container = styled.div`
   max-width: 80em;
@@ -28,5 +43,5 @@ Favorite.Container = styled.div`
 
 Favorite.Wrapper = styled.div`
   display: flex;
-  padding: 1em;
+  padding: 2.5em;
 `;
